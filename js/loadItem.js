f5f921f0377255b39d00b875055167008ce49ea6
HeeyeonJeong <junghy14971@gmail.com>
2020-12-21 22:22:19 +0900

    Feat: save goods to wishlist

--- js/loadItem.js
+++ js/loadItem.js
@@ -1,46 +1,90 @@
 const itemContainer = document.querySelector(".goods-container");
+const wishContainer = document.querySelector(".wish-container");
 
 //JSON fetch
-function loadItems() {
-  return fetch("./data/data.json")
-    .then((response) => response.json())
-    .then((json) => json.shoesBox);
+async function loadItems() {
+  const response = await fetch("./data/data.json");
+  const json = await response.json();
+  return json.shoesBox;
 }
 
 //list출력
 function displayItems(shoesBox) {
-  itemContainer.innerHTML = shoesBox.map((shoes) => createHTML(shoes)).join("");
+  if (itemContainer !== null) {
+    itemContainer.innerHTML = shoesBox
+      .map((shoes) => createHTML(shoes))
+      .join("");
+  }
+  loadWish(shoesBox);
 }
 
 //list HTML
 function createHTML(shoes) {
   return `
     <li class="goods-card">
-        <a href="#" class="goods-item-link">
-            <div class="card-img-box">
-                <img src="${shoes.image}" alt="${shoes.productName}" class="card-img">
-            </div>
-            <div class="card-info">
-                <div class="card-title">
-                    <p>${shoes.productName}</p>
-                </div>
-                <div class="card-precis">
-                    <span class="card-price">${shoes.price}</span>
-                    <button type="button" class="card-icon"><i class='bx bx-heart'></i></button>
-                    <button type="button" class="card-icon"><i class='bx bx-cart'></i></button>
-                </div>        
-            </div>
-        </a>
+      <div class="card-img-box">
+          <img src="${shoes.image}" alt="${shoes.productName}" class="card-img">
+      </div>
+      <div class="card-info">
+          <div class="card-title">
+              <p>${shoes.productName}</p>
+          </div>
+          <div class="card-precis">
+              <span class="card-price">${shoes.price}</span>
+              <button type="button" data-id=${shoes.id} class="card-icon like-icon"><i class='bx bx-heart'></i></button>
+              <button type="button" data-id=${shoes.id} class="card-icon cart-icon"><i class='bx bx-cart'></i></button>
+              <a href="wishlist.html" data-id=${shoes.id} class="card-icon more-icon"><i class='bx bxs-plus-square'></i></a>
+          </div>               
+      </div>        
     </li>
 `;
+}
+
+//save wish
+function saveWish(wishObj) {
+  localStorage.setItem("wishList", JSON.stringify(wishObj));
+}
+
+// paint wish list
+function paintWishPage() {
+  const loadWishGoods = localStorage.getItem("wishList");
+  if (wishContainer !== null) {
+    wishContainer.innerHTML = JSON.parse(loadWishGoods)
+      .map((shoes) => createHTML(shoes))
+      .join("");
+  }
+}
+
+//wish-storage
+let wishObj = localStorage.getItem("wishList")
+  ? JSON.parse(localStorage.getItem("wishList"))
+  : [];
+
+//wish goods
+function loadWish(shoesBox) {
+  const likebtns = document.querySelectorAll(".like-icon");
+  likebtns.forEach((likebtn) => {
+    likebtn.addEventListener("click", (e) => {
+      const goodsBtn = e.target.parentNode;
+      const userWish = shoesBox.find((shoes) => {
+        if (shoes.id === parseInt(goodsBtn.dataset.id)) {
+          return shoes;
+        }
+      });
+      wishObj.push(userWish);
+      saveWish(wishObj);
+    });
+  });
 }
 
 //user selected color
 function selectHandler(shoesBox) {
   const sortContainer = document.querySelector(".goods-sort");
-  sortContainer.addEventListener("change", (e) =>
-    selectColorFilter(e, shoesBox)
-  );
+  if (sortContainer !== null) {
+    sortContainer.addEventListener("change", (e) =>
+      selectColorFilter(e, shoesBox)
+    );
+  }
 }
 
 //color filtering
@@ -58,4 +102,5 @@ function selectColorFilter(e, shoesBox) {
 loadItems().then((shoesBox) => {
   displayItems(shoesBox);
   selectHandler(shoesBox);
+  paintWishPage();
 });
